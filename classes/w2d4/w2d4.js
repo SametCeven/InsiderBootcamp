bc933dfe9102fa334a65d3f9f7a5642966fab7be
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-07-11 20:02:28 +0300

    class w2d4 added

---
+++ classes/w2d4/w2d4.js
@@ -0,0 +1,18 @@
+const container = document.getElementsByClassName("container")[0]
+container.setAttribute("deneme","test")
+container.removeAttribute("deneme")
+
+const div = document.createElement("div")
+div.className = "new-child"
+
+//container.replaceChild(div,container.firstElementChild);
+
+container.addEventListener("click",(e)=>{
+    console.log(e.target)
+
+})
+
+
+
+
+
