fbb5f7f806db89055d947adedad648e4e285b7bb
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-07-12 19:29:05 +0300

    form validation added

--- projects/w2/index.js
+++ projects/w2/index.js
@@ -12,72 +12,117 @@ const currentTaskSection = document.querySelector(".task-section");
 
 
 // FORM INPUT EVENT
-form.addEventListener("change",(e)=>{
-    if(e.target === formTitle){
+form.addEventListener("change", (e) => {
+    if (e.target === formTitle) {
         formData["formTitle"] = e.target.value;
-    }else if(e.target === formDescription){
+    } else if (e.target === formDescription) {
         formData["formDescription"] = e.target.value;
-    }else if(e.target.type === "radio"){
+    } else if (e.target.type === "radio") {
         formData["formRadio"] = e.target.value;
     }
 })
 
 
 // FORM BUTTON SUBMIT EVENT
-formButton.addEventListener("click",(e)=>{
+formButton.addEventListener("click", (e) => {
     e.preventDefault();
-    formTitle.value = "";
-    formTitle.textContent = "";
-    formDescription.value = "";
-    formDescription.textContent = "";
-    formRadio.value = "";
-    formAllRadios.forEach((r)=>{
-        r.checked = false;
-    })
-    
-    const div = document.createElement("div");
-    div.classList.add("task")
-    
-    const h2 = document.createElement("h2");
-    h2.value = formData.formTitle,
-    h2.textContent = formData.formTitle;
-    
-    const p = document.createElement("p");
-    p.value = formData.formDescription;
-    p.textContent = formData.formDescription;
 
-    const infoContainer = document.createElement("div");
-    infoContainer.classList.add("info-container")
+    try {
+        // FORM VALIDATION
+        const errors = [];
 
-    const span = document.createElement("span");
-    span.value = formData.formRadio;
-    span.textContent = formData.formRadio;
+        if (!formData.formTitle) errors.push("Title is required!")
 
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox";
+        if (!formData.formDescription) errors.push("Description is required!")
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "-"
+        if (!formData.formRadio) errors.push("Priority is required!")
 
-    infoContainer.appendChild(span);
-    infoContainer.appendChild(inputCheckbox);
-    infoContainer.appendChild(deleteButton);
+        if (errors.length > 0) throw new Error(errors.join(""));
 
-    div.appendChild(infoContainer);
-    div.appendChild(h2);
-    div.appendChild(p);
-    currentTaskSection.appendChild(div);
 
-    formData = {};
+        // RESETTING FORM
+        formTitle.value = "";
+        formTitle.textContent = "";
+        formDescription.value = "";
+        formDescription.textContent = "";
+        formRadio.value = "";
+        formAllRadios.forEach((r) => {
+            r.checked = false;
+        })
+        const errContainer = document.querySelector(".error-container")
+        if (errContainer) errContainer.remove();
+
+
+        // ADDING TASK
+        const div = document.createElement("div");
+        div.classList.add("task")
+
+        const h2 = document.createElement("h2");
+        h2.value = formData.formTitle,
+            h2.textContent = formData.formTitle;
+
+        const p = document.createElement("p");
+        p.value = formData.formDescription;
+        p.textContent = formData.formDescription;
+
+        const infoContainer = document.createElement("div");
+        infoContainer.classList.add("info-container")
+
+        const span = document.createElement("span");
+        span.value = formData.formRadio;
+        span.textContent = formData.formRadio;
+
+        const inputCheckbox = document.createElement("input");
+        inputCheckbox.type = "checkbox";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "-"
+
+        infoContainer.appendChild(span);
+        infoContainer.appendChild(inputCheckbox);
+        infoContainer.appendChild(deleteButton);
+
+        div.appendChild(infoContainer);
+        div.appendChild(h2);
+        div.appendChild(p);
+        currentTaskSection.appendChild(div);
+
+        formData = {};
+
+    } catch (err) {
+        errors = err.message.split("!");
+
+        let errContainer = document.querySelector(".error-container");
+
+        if (!errContainer) {
+            errContainer = document.createElement("ul");
+        }
+
+        errContainer.classList.add("error-container");
+        form.appendChild(errContainer);
+
+        errContainer.innerHTML = "";
+
+        errors.forEach((e) => {
+            const errMessage = document.createElement("li");
+            errMessage.classList.add("error-message");
+            errMessage.textContent = e;
+            errContainer.appendChild(errMessage);
+        })
+    }
 })
 
 
 
 // CURRENT TASK ELEMENTS
-currentTaskSection.addEventListener("click",((e)=>{
-    if(e.target.tagName === "BUTTON"){
+currentTaskSection.addEventListener("click", ((e) => {
+    if (e.target.tagName === "BUTTON") {
         e.stopPropagation();
         const task = e.target.closest(".task");
         task.remove();
     }
 }))
+
+
+
+
