37f09828ac548b67eae4e9a3ee81841612be68d8
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-07-16 10:00:07 +0300

    fetch logic added

---
+++ projects/w3d2/index.js
@@ -0,0 +1,75 @@
+
+$(document).ready(() => {
+
+    let posts = [];
+
+    // LOADING & ERROR STATE MANAGEMENT
+    let loadingState = false;
+    let errorState = null;
+
+    const $loadingElement = `<div class="loading"> Loading ... </div>`
+
+    $(document)
+        .ajaxStart(() => {
+            loadingState = true;
+            $("#post-container").append($loadingElement);
+        }).ajaxSuccess(() => {
+            errorState = false;
+        }).ajaxError(() => {
+            errorState = true;
+        }).ajaxComplete(() => {
+            loadingState = false;
+            $(".loading").remove();
+        });
+
+
+    // UTIL FUNCTION FOR ADDING DATA TO DOM
+    function addPosts() {
+        posts.forEach((post) => {
+            const $postItem =
+                `
+                <div class="post-item">
+                    <h2> ${post.title} </h2>
+                    <p> ${post.body} </p>
+                </div>
+                `;
+            $("#post-container").append($postItem);
+        })
+    }
+
+
+
+    // GETTING DATA FROM JSONPLACEHOLDER
+
+
+    function getData(limit, offset) {
+        const baseUrl = `https://jsonplaceholder.typicode.com/posts/?_limit=${limit}&_start=${offset}`;
+
+        $.ajax({
+            url: baseUrl,
+            method: "GET",
+        }).done((res) => {
+            posts.push(...res);
+            addPosts();
+            console.log(res);
+        }).fail((err) => {
+            console.error(err);
+        }).always(() => {
+            console.log(123);
+        })
+    }
+
+    let limit = 5;
+    let offset = 0;
+    getData(limit, offset);
+
+
+
+
+
+
+
+
+
+
+})
