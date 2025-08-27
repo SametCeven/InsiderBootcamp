0573348c7f727e2c56098323ed325fb10036fc05
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-08-27 16:04:47 +0300

    barcin added

--- tasks/t3/index.js
+++ tasks/t3/index.js
@@ -25,7 +25,7 @@ main = ($) => {
 
     const config = {
         lcw: {
-            url: ["https://www.lcw.com"],
+            url: ["www.lcw.com"],
             partnerName: "lcw",
             pageType: {
                 mainPage: "Home",
@@ -35,7 +35,7 @@ main = ($) => {
             },
         },
         turkcell: {
-            url: ["https://www.turkcell.com.tr", "https://www.superonline.net/"],
+            url: ["www.turkcell.com.tr", "www.superonline.net/"],
             partnerName: "turkcell",
             pageType: {
                 mainPage: 5,
@@ -45,8 +45,14 @@ main = ($) => {
             }
         },
         barcin: {
-            url: ["https://www.barcin.com"],
+            url: ["www.barcin.com"],
             partnerName: "barcin",
+            pageType: {
+                mainPage: [/web-home/],
+                cartPage: "basket",
+                productPage: [/product-add-to-cart/],
+                categoryPage: [/product-info/],
+            }
         }
     }
 
@@ -64,8 +70,7 @@ main = ($) => {
     }
 
     self.reset = () => {
-        $(selectors.style).remove();
-        $(selectors.container).empty();
+        $(selectors.container).remove();
     }
 
     self.buildHTML = () => {
@@ -76,7 +81,7 @@ main = ($) => {
     // ---- LOGIC ---- //
 
     self.getInitialInfo = () => {
-        self.currentURL = document.URL;
+        self.currentURL = window.location.hostname;
         self.currentPageType = window.CurrentPageType;
 
         const partner = Object.values(config).find((p) => {
@@ -168,33 +173,69 @@ main = ($) => {
 
     // ---- BARCIN --- //
 
+    self.barcinIsOnMainPage = () => {
+        if (!self.checkPartner("barcin")) return false;
+        const $matches = $(document).find(`*[id]`).filter(function(){
+            return config.barcin.pageType.mainPage.some((regex)=>{
+                return regex.test(this.id);
+            })
+        })
+        if($matches.length >= 1) return true;
+        return false;
+    }
+
+    self.barcinIsOnCartPage = () => {
+        if (!self.checkPartner("barcin")) return false;
+        const pathname = window.location.pathname;
+        if(pathname.includes(config.barcin.pageType.cartPage)) return true;
+        return false;
+    }
+
+    self.barcinIsOnProductPage = () => {
+        const $matches = $(document).find(`*[data-testid]`).filter(function(){
+            return config.barcin.pageType.productPage.some((regex)=>{
+                return regex.test(this.getAttribute("data-testid"));
+            })
+        })
+        if($matches.length >= 1) return true;
+        return false;
+    }
+
+    self.barcinIsOnCategoryPage = () => {
+        if (!self.checkPartner("barcin")) return false;
+        const $matches = $(document).find(`*[class]`).filter(function(){
+            return config.barcin.pageType.categoryPage.some((regex)=>{
+                return regex.test(this.className);
+            })
+        })
+        if($matches.length >= 1) return true;
+        return false;
+    }
 
     // --- TEST --- //
 
     self.test = () => {
-        console.log("********")
+        /* console.log("********")
         console.log($(document))
         console.log(document)
         console.log(window)
-        //console.log(document.URL)
-        //console.log(self.URL);
-        //console.log(self.pageType);
-        console.log("********")
-        /* console.log($(document).find("#__NEXT_DATA__"))
-        console.log($(document).find("#__NEXT_DATA__")[0].innerHTML)
-        console.log(JSON.parse($(document).find("#__NEXT_DATA__")[0].innerHTML).query.slug) */
+        console.log(window.location.pathname)
+        console.log("********") */
 
-        console.log("********")
-
-        /* console.log("lcw /", "main page /", self.lcwIsOnMainPage());
+        console.log("lcw /", "main page /", self.lcwIsOnMainPage());
         console.log("lcw /", "product page /", self.lcwIsOnProductPage());
         console.log("lcw /", "category page /", self.lcwIsOnCategoryPage());
-        console.log("lcw /", "cart page /", self.lcwIsOnCartPage()); */
+        console.log("lcw /", "cart page /", self.lcwIsOnCartPage());
 
         console.log("turkcell /", "main page /", self.turkcellIsOnMainPage());
         console.log("turkcell /", "product page /", self.turkcellIsOnProductPage());
         console.log("turkcell /", "category page /", self.turkcellIsOnCategoryPage());
         console.log("turkcell /", "cart page /", self.turkcellIsOnCartPage());
+
+        console.log("barcin /", "main page /", self.barcinIsOnMainPage());
+        console.log("barcin /", "product page /", self.barcinIsOnProductPage());
+        console.log("barcin /", "category page /", self.barcinIsOnCategoryPage());
+        console.log("barcin /", "cart page /", self.barcinIsOnCartPage());
 
     }
 
@@ -204,6 +245,7 @@ main = ($) => {
     window.fn = {
         "lcw": { isOnCartPage: self.lcwIsOnCartPage, isOnProductPage: self.lcwIsOnProductPage, isOnMainPage: self.lcwIsOnMainPage, isOnCategoryPage: self.lcwIsOnCategoryPage },
         "turkcell": { isOnCartPage: self.turkcellIsOnCartPage, isOnProductPage: self.turkcellIsOnProductPage, isOnMainPage: self.turkcellIsOnMainPage, isOnCategoryPage: self.turkcellIsOnCategoryPage },
+        "barcin": { isOnCartPage: self.barcinIsOnCartPage, isOnProductPage: self.barcinIsOnProductPage, isOnMainPage: self.barcinIsOnMainPage, isOnCategoryPage: self.barcinIsOnCategoryPage },
     }
 
     $(document).ready(self.init);
