9e56539c4d0169e14139e954fd0cc04251bcd480
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-08-27 12:26:01 +0300

    bug fix
    - lcw isOnCategoryPage fixed
    - checkPartner helper added

--- tasks/t3/index.js
+++ tasks/t3/index.js
@@ -24,19 +24,27 @@ main = ($) => {
     );
 
     const self = {
-        partners: ["https://www.lcw.com", "turkcell", "barcin"],
-        URL: "",
-        pageType: "",
+        partners: ["https://www.lcw.com", "https://www.turkcell.com.tr", "barcin"],
         config: {
             lcw: {
+                partnerName: "lcw",
                 pageType: {
                     mainPage: "Home",
                     cartPage: "Shop",
                     productPage: "Model",
                     categoryPage: ".product-list-container",
-                }
+                },
+            },
+            turkcell: {
+                partnerName: "turkcell",
+            },
+            barcin: {
+                partnerName: "barcin",
             }
-        }
+        },
+        currentPartner : "",
+        URL: "",
+        pageType: "",
     }
 
     self.init = () => {
@@ -58,10 +66,16 @@ main = ($) => {
     // ---- LOGIC ---- //
 
     self.getInitialInfo = () => {
-        
         self.URL = document.URL;
         self.pageType = window.CurrentPageType;
-
+        
+        if(self.URL.includes(self.config.lcw.partnerName)){
+            self.currentPartner = self.config.lcw.partnerName;
+        }else if(self.URL.includes(self.config.turkcell.partnerName)){
+            self.currentPartner = self.config.turkcell.partnerName;
+        }else if(self.URL.includes(self.config.barcin.partnerName)){
+            self.currentPartner = self.config.barcin.partnerName;
+        }
 
         console.log($(document))
         console.log(document)
@@ -80,35 +94,80 @@ main = ($) => {
         return isPartner;
     }
 
+    self.checkPartner = (partner) => {
+        if(!self.isPartner()){
+            console.error("Not a partner !!!");
+            return false;
+        }
+        if(self.currentPartner !== self.config[partner].partnerName){
+            console.error("Wrong partner !!!");
+            return false;
+        }
+        return true;
+    }
+
+    // ---- LCW --- //
+
     self.lcwIsOnMainPage = () => {
-        if(!self.isPartner()) return "not a partner";
+        if(!self.checkPartner("lcw")) return false;
         if(self.pageType === self.config.lcw.pageType.mainPage) return true;
         else return false;
     }
 
     self.lcwIsOnCartPage = () => {
-        if(!self.isPartner()) return "not a partner";
+        if(!self.checkPartner("lcw")) return false;
         if(self.pageType === self.config.lcw.pageType.cartPage) return true;
         else return false;
     }
 
     self.lcwIsOnProductPage = () => {
-        if(!self.isPartner()) return "not a partner";
+        if(!self.checkPartner("lcw")) return false;
         if(self.pageType === self.config.lcw.pageType.productPage) return true;
         else return false;
     }
 
     self.lcwIsOnCategoryPage = () => {
-        if(!self.isPartner()) return "not a partner";
+        if(!self.checkPartner("lcw")) return false;
         
-        const categoryPage = $(document).find(self.config.lcw.pageType.categoryPage)
-        if(!categoryPage) return false;
+        const $categoryPage = $(document).find(self.config.lcw.pageType.categoryPage);
+        if($categoryPage.length < 1) return false;
         self.pageType = self.config.lcw.pageType.categoryPage;
         return true;
     }
 
+
+    // ---- TURKCELL --- //
+
+    self.turkcellIsOnMainPage = () => {
+        if(!self.checkPartner("turkcell")) return false;
+        return "main page"
+    }
+
+    self.turkcellIsOnCartPage = () => {
+        if(!self.checkPartner("turkcell")) return false;
+        return "cart page"
+    }
+
+    self.turkcellIsOnProductPage = () => {
+        if(!self.checkPartner("turkcell")) return false;
+        return "product page"
+    }
+
+    self.turkcellIsOnCategoryPage = () => {
+        if(!self.checkPartner("turkcell")) return false;
+        return "category page"
+    }
+
+    // ---- BARCIN --- //
+
+
+
+
+    // ---- FN --- //
+
     window.fn = {
-        "lcw": { isOnCartPage: self.lcwIsOnCartPage, isOnProductPage: self.lcwIsOnProductPage, isOnMainPage: self.lcwIsOnMainPage, lcwIsOnCategoryPage: self.lcwIsOnCategoryPage }
+        "lcw": { isOnCartPage: self.lcwIsOnCartPage, isOnProductPage: self.lcwIsOnProductPage, isOnMainPage: self.lcwIsOnMainPage, isOnCategoryPage: self.lcwIsOnCategoryPage },
+        "turkcell": { isOnCartPage: self.turkcellIsOnCartPage, isOnProductPage: self.turkcellIsOnProductPage, isOnMainPage: self.turkcellIsOnMainPage, isOnCategoryPage: self.turkcellIsOnCategoryPage },
     }
 
     $(document).ready(self.init);
