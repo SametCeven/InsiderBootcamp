198d295da365a318374558ea1f659d87f1ff3eaa
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-08-27 11:39:11 +0300

    lcw function added

--- tasks/t3/index.js
+++ tasks/t3/index.js
@@ -21,40 +21,95 @@ main = ($) => {
         appendLocation: `body`,
         body: `body`,
     }
-
     );
 
     const self = {
-       
+        partners: ["https://www.lcw.com", "turkcell", "barcin"],
+        URL: "",
+        pageType: "",
+        config: {
+            lcw: {
+                pageType: {
+                    mainPage: "Home",
+                    cartPage: "Shop",
+                    productPage: "Model",
+                    categoryPage: ".product-list-container",
+                }
+            }
+        }
     }
-
 
     self.init = () => {
         self.reset();
         self.buildHTML();
-        self.setEvents();
+        self.getInitialInfo();
     }
 
     self.reset = () => {
         $(selectors.style).remove();
-        $(document).off(".eventListener");
         $(selectors.container).empty();
     }
 
     self.buildHTML = () => {
-        const html =`<div class=${classes.container}></div>`
+        const html = `<div class=${classes.container}></div>`
         $(selectors.appendLocation).append(html);
     }
 
-    self.setEvents = () => {
-       
+    // ---- LOGIC ---- //
+
+    self.getInitialInfo = () => {
+        
+        self.URL = document.URL;
+        self.pageType = window.CurrentPageType;
+
+
+        console.log($(document))
+        console.log(document)
+        console.log(window)
+        console.log(document.URL)
+        console.log(self.URL);
+        console.log(self.pageType);
     }
 
-    console.log("asd")
-   
 
+    self.isPartner = () => {
+        let isPartner = false;
+        self.partners.forEach((p)=>{
+            if(self.URL.includes(p)) isPartner = true;
+        })
+        return isPartner;
+    }
+
+    self.lcwIsOnMainPage = () => {
+        if(!self.isPartner()) return "not a partner";
+        if(self.pageType === self.config.lcw.pageType.mainPage) return true;
+        else return false;
+    }
+
+    self.lcwIsOnCartPage = () => {
+        if(!self.isPartner()) return "not a partner";
+        if(self.pageType === self.config.lcw.pageType.cartPage) return true;
+        else return false;
+    }
+
+    self.lcwIsOnProductPage = () => {
+        if(!self.isPartner()) return "not a partner";
+        if(self.pageType === self.config.lcw.pageType.productPage) return true;
+        else return false;
+    }
+
+    self.lcwIsOnCategoryPage = () => {
+        if(!self.isPartner()) return "not a partner";
+        
+        const categoryPage = $(document).find(self.config.lcw.pageType.categoryPage)
+        if(!categoryPage) return false;
+        self.pageType = self.config.lcw.pageType.categoryPage;
+        return true;
+    }
+
+    window.fn = {
+        "lcw": { isOnCartPage: self.lcwIsOnCartPage, isOnProductPage: self.lcwIsOnProductPage, isOnMainPage: self.lcwIsOnMainPage, lcwIsOnCategoryPage: self.lcwIsOnCategoryPage }
+    }
 
     $(document).ready(self.init);
-
-
 }
